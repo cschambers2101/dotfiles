f46ce4909d69ced938d05bd9ae18bcf1db57fec2
Craig Chambers <cchambers@salisbury6c.ac.uk>
2021-03-16 11:39:34 +0000

    Updated to use neovim COC

--- install.sh
+++ install.sh
@@ -18,15 +18,22 @@ sudo apt -y install neofetch
 echo 'complete'
 
 echo
-echo 'Installing Vundle plugin manager for Vim ... '
+
+echo 'Installing Vim Plug'
+
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+echo 'Complete!'
+
+# echo 'Installing Vundle plugin manager for Vim ... '
 
 # Install Vundle to allow plugins to work
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-echo 'complete'
+# git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+# echo 'complete'
 
-echo 'You must run PluginInstall from within VIM to setup your plugins'
-echo
-echo 'Installing local dot files ... '
+# echo 'You must run PluginInstall from within VIM to setup your plugins'
+# echo
+# echo 'Installing local dot files ... '
 
 # create local dotfiles
 cp bashrc ~/.bashrc
@@ -40,25 +47,27 @@ if [ ! -d "~/.config/powerline" ]; then
 fi
 cp powerline ~/.config/powerline/config.json
 echo 'complete'
-echo
-echo 'Vim text editor will now launch and install the plugins'
-echo
-echo 'You will need to exit from Vim twice! Hit these keys to exit Vim'
-echo '[esc], :, q, [esc], :, q'
-echo 'Ignore the plugin error!'
-echo
 
-read -p "Press any key to continue... " -n1 -s
 
-vim -c "PluginInstall"
-
-echo 'complete'
-# echo 'After running PluginInstall in Vim, run the following commands'
 # echo
-echo 'Setting up YouCompleteMe'
-cd ~/.vim/bundle/YouCompleteMe
-python3 install.py
-echo 'complete'
+# echo 'Vim text editor will now launch and install the plugins'
+# echo
+# echo 'You will need to exit from Vim twice! Hit these keys to exit Vim'
+# echo '[esc], :, q, [esc], :, q'
+# echo 'Ignore the plugin error!'
+# echo
+
+# read -p "Press any key to continue... " -n1 -s
+
+# vim -c "PluginInstall"
+
+# echo 'complete'
+# # echo 'After running PluginInstall in Vim, run the following commands'
+# # echo
+# echo 'Setting up YouCompleteMe'
+# cd ~/.vim/bundle/YouCompleteMe
+# python3 install.py
+# echo 'complete'
 
 clear
 

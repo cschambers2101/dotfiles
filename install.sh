ef29e6cd7466b0904c5550f9741df486b3fb7281
Craig Chambers <craigchambers2101@gmail.com>
2022-04-14 12:28:00 +0100

    Moved vimplug download to earlier in the script

--- install.sh
+++ install.sh
@@ -5,17 +5,21 @@ sudo apt -y install neofetch curl
 sudo snap install starship
 echo 'complete'
 
+echo
+echo 'Download VimPlug'
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+echo 'complete'
+
+echo
+echo 'Creating local dotfiles'
 # create local dotfiles
 cp bashrc ~/.bashrc
 cp bash_aliases ~/.bash_aliases
 cp bash_functions ~/.bash_functions
 cp vimrc ~/.vimrc
-
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-
-
 echo 'complete'
+
 echo
 echo
 echo 'Your dotfiles are installed and setup'

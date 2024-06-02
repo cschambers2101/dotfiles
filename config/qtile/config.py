6bfda38461916fcccb20681c5c69f0cb3312a35f
Craig Chambers <craigchambers2101@gmail.com>
2024-06-02 10:36:36 +0100

    Added launch terminal and browser to qtile config

--- config/qtile/config.py
+++ config/qtile/config.py
@@ -43,32 +43,6 @@ mod = "mod4"
 """
 Color schemes live here
 """
-_jagl = {
-	'bg':   		'#1e1e2e',
-	'fg':			'#cdd6f4',
-    'dark-red':     '#eba0ac',
-    'red':          '#f38ba8',
-    'dark-green':   '#94e2d5',
-    'green':        '#a6e3a1',
-    'dark-yellow':  '#fab387',
-    'yellow':       '#f9e2af',
-    'dark-blue':    '#74c7ec',
-    'blue':         '#89b4fa',
-    'dark-magenta': '#cba6f7',
-    'magenta':      '#f5c2e7',
-    'dark-cyan':    '#94e2d5',
-    'cyan':         '#89dceb',
-    'dark-gray':    '#7f849c',
-    'gray':         '#9399b2',
-
-    'fg4':          '#7f849c',
-    'fg3':          '#6c7086',
-    'fg2':          '#585b70',
-    'fg1':          '#45475a',
-    'bg0':          '#313244',
-    'fg0':          '#181825',
-    'fg9':          '#bac2de'
-}
 
 _gruvbox = {
     'bg':           '#282828',
@@ -189,6 +163,11 @@ keys = [
 # TOGGLE FLOATING LAYOUT
     Key([mod, "shift"], "space", lazy.window.toggle_floating()),
 	Key([mod, "shift"], "z", lazy.layout.normalize(), desc="Reset all window sizes"),
+
+# LAUNCH PROGRAMS
+    Key([mod], "b", lazy.spawn("google-chrome")), # Launch Chrome
+    Key([mod], "Return", lazy.spawn("gnome-terminal")) # Launch Gnome Terminal
+
 
     ]
 # end of keys
